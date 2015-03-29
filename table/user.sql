4b528ff08d71509ca0605b3ee52f9f4588f164f7
juszczakn <juszczakn@gmail.com>
2015-03-29 12:40:01 -0500

    email must be unique

--- table/user.sql
+++ table/user.sql
@@ -2,6 +2,6 @@
 create table if not exists users (
        userId serial primary key,
        name varchar(30) not null,
-       email varchar(50) not null,
+       email varchar(50) unique not null,
        password varchar(128) not null
 );
